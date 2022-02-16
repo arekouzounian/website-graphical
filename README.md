26a298de6f305aee07f1742f7367e106f801a2f9
Arek Ouzounian <agouzo777@gmail.com>
2022-02-16 01:31:40 -0800

    First version of GUI website--additions to come (hopefully)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-now with GUI!
+# Personal website - now with GUI!
+
+As the last website's design centered around trying to emulate the command line, 
+I figured that I would make a secondary website that would be more intuitive for people
+that aren't familiar with the command line. 
---
+++ src/projects.js
@@ -0,0 +1,65 @@
+var interests = ["Web App Security", "Bug Bounties", "Ethical Hacking/Pentesting", "CTFs", "Go/Rust", "Command-Line Tools", "Docker/Containerization"];
+var cycle = 0;
+
+function print(rand = true)
+{
+    var ul = document.getElementById("interests");
+    if(ul.innerHTML != "")
+    {
+        clear();
+        if (rand)
+        {
+            randomize(interests);
+        }
+    }
+        
+
+    for( var i = 0; i < interests.length; i++ ) {
+        var li = document.createElement('li');
+        li.classList.add('fade-in');
+        li.textContent = interests[i];
+        ul.appendChild(li);
+    }
+}
+
+function randomize(lst)
+{
+    for(var i = 0; i < lst.length; i++)
+    {
+        var ind = Math.floor(Math.random() * lst.length);
+        var tmp = lst[i];
+        lst[i] = lst[ind];
+        lst[ind] = tmp;
+    }
+}
+
+
+function clear()
+{
+    document.getElementById("interests").innerHTML = "";
+}
+
+function sort()
+{
+    switch(cycle)
+    {
+        case 0:
+            interests.sort();
+            break;
+        case 1:
+            interests.sort(function(a, b) { return a.length > b.length; });
+            break;
+        case 2:
+            interests.sort(function(a, b) { return a.length < b.length; });
+            break;
+    }
+    print(false);
+    cycle++;
+
+    if(cycle > 2)
+    {
+        cycle = 0; 
+    }
+}
+
+print();
